fbd1b74a4ee9e14f52bbd6efaebef537092d6c3e
Kaspar Alexander <kaspar.alexander@gmail.com>
2022-12-31 15:29:35 -0800

    modal, modal tests, and fixed react version for image rendering

--- src/components/Contact/__tests__/index.test.js
+++ src/components/Contact/__tests__/index.test.js
@@ -2,11 +2,11 @@
 import React from 'react'
 import { render, cleanup } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
-import Contact from './Contact'
+import Contact from '..'
 
 afterEach(cleanup)
 
-describe('Contact component renders', () => {
+describe('Contact component', () => {
     it('renders', () => {
         render(<Contact />);
     });
